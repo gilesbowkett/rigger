b9c6daa0e0efc894a524a98448066ffc18484e3e
James Golick <jamesgolick@gmail.com>
2011-03-03 16:18:41 -0800

    implement :only

--- lib/rigger/server_resolver.rb
+++ lib/rigger/server_resolver.rb
@@ -6,15 +6,29 @@ module Rigger
 
     def call(task)
       roles = [*task.options[:role]].compact
-      if roles.empty?
-        @config.servers
-      else
+      only  = task.options.fetch(:only, {})
+
+      role_servers = roles.empty? ? @config.servers : from_roles(roles)
+      only_servers = only.empty?  ? @config.servers : from_only(only)
+
+      role_servers & only_servers
+    end
+
+    protected
+      def from_roles(roles)
         roles.map do |role|
           @config.servers.select do |server|
             server.role == role
           end
         end.flatten
       end
-    end
+
+      def from_only(only)
+        @config.servers.select do |server|
+          only.all? do |key, value|
+            server.options[key] == value
+          end
+        end
+      end
   end
 end
--- spec/server_resolver_spec.rb
+++ spec/server_resolver_spec.rb
@@ -5,7 +5,10 @@ describe "Rigger::ServerResolver" do
   before do
     @server1  = stub("Server", :options => {}, :role => :db)
     @server2  = stub("Server", :options => {}, :role => :app)
-    @servers  = [@server1, @server2]
+    @server3  = stub("Server", :options => {
+      :fuck => true
+    }, :role => :search)
+    @servers  = [@server1, @server2, @server3]
     @config   = stub("Config", :servers => @servers)
     @resolver = Rigger::ServerResolver.new(@config)
   end
@@ -27,6 +30,16 @@ describe "Rigger::ServerResolver" do
 
     it "returns all the servers matching that role" do
       @resolver.call(@task).should == [@server2]
+    end
+  end
+
+  describe "when the task specifies an option" do
+    before do
+      @task = stub("Config", :options => {:only => {:fuck => true}})
+    end
+
+    it "returns all the servers matching that role" do
+      @resolver.call(@task).should == [@server3]
     end
   end
 end
