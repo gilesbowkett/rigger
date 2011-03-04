088f2f4b637cff3cefe4c4c7b04736f67ef97e2d
James Golick <jamesgolick@gmail.com>
2011-03-03 16:10:55 -0800

    actually making server resolution work

--- lib/rigger/server_resolver.rb
+++ lib/rigger/server_resolver.rb
@@ -5,8 +5,16 @@ module Rigger
     end
 
     def call(task)
-      # TODO: Might wanna implement this.
-      @config.servers
+      roles = [*task.options[:role]].compact
+      if roles.empty?
+        @config.servers
+      else
+        roles.map do |role|
+          @config.servers.select do |server|
+            server.role == role
+          end
+        end.flatten
+      end
     end
   end
 end
--- spec/server_resolver_spec.rb
+++ spec/server_resolver_spec.rb
@@ -1,12 +1,32 @@
 require "spec_helper"
+require "rigger/server_resolver"
 
 describe "Rigger::ServerResolver" do
   before do
-    @server = stub("Server", :options => {})
-    @config = stub("Config", :servers => [@server])
+    @server1  = stub("Server", :options => {}, :role => :db)
+    @server2  = stub("Server", :options => {}, :role => :app)
+    @servers  = [@server1, @server2]
+    @config   = stub("Config", :servers => @servers)
+    @resolver = Rigger::ServerResolver.new(@config)
   end
 
-  it "flunks" do
-    true.should == false
+  describe "when the task has no options" do
+    before do
+      @task = stub("Config", :options => {})
+    end
+
+    it "returns all the servers" do
+      @resolver.call(@task).should == @servers
+    end
+  end
+
+  describe "when the task specifies a role" do
+    before do
+      @task = stub("Config", :options => {:role => :app})
+    end
+
+    it "returns all the servers matching that role" do
+      @resolver.call(@task).should == [@server2]
+    end
   end
 end
