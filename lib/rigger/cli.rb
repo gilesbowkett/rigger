0b207ec77cf52993421e46e6cbc8c148b505a279
James Golick <jamesgolick@gmail.com>
2011-03-04 11:25:51 -0800

    display tasks with -T. we don't have descriptions yet, but when we do, we'll put them there I guess

--- lib/rigger/cli.rb
+++ lib/rigger/cli.rb
@@ -1,5 +1,6 @@
 require "rigger/dsl"
 require "rigger/task_execution_service"
+require "optparse"
 
 module Rigger
   class CLI
@@ -12,17 +13,23 @@ module Rigger
     end
 
     def start
-      @config_file = locate_config_file
-      @config      = @dsl.new
-      @config.load_from_file(@config_file)
-      executor     = @task_execution_service_factory.new(@config)
+      options = parse_options
+      load_config_file
 
-      @args.each do |task_name|
-        executor.call(task_name)
+      if options[:display_tasks]
+        display_tasks
+      else
+        run_tasks
       end
     end
 
     protected
+      def load_config_file
+        @config_file = locate_config_file
+        @config      = @dsl.new
+        @config.load_from_file(@config_file)
+      end
+
       def locate_config_file
         location = possible_config_locations.detect { |l| File.exist?(l) }
         location || raise_missing_config_file
@@ -34,6 +41,31 @@ module Rigger
 
       def raise_missing_config_file
         raise "Couldn't find a config file in #{possible_config_locations.inspect}"
+      end
+
+      def run_tasks
+        executor = @task_execution_service_factory.new(@config)
+
+        @args.each do |task_name|
+          executor.call(task_name)
+        end
+      end
+
+      def parse_options
+        {}.tap do |options|
+          OptionParser.new do |opts|
+            opts.banner = "Usage: #{$0} [options]"
+            opts.on("-T", "--tasks", "Display tasks.") do
+              options[:display_tasks] = true
+            end
+          end.parse!
+        end
+      end
+
+      def display_tasks
+        @config.tasks.sort_by { |t| t.name }.each do |task|
+          puts "rig #{task.name}"
+        end
       end
   end
 end
