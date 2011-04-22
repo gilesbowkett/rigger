ea6d4608b9bdf49e61b0f733b8e105d3d0d84555
James Golick <jamesgolick@gmail.com>
2011-04-22 11:40:40 -0700

    fuck me

--- lib/rigger/recipes/chef.rb
+++ lib/rigger/recipes/chef.rb
@@ -1,17 +1,3 @@
-namespace :service do
-  task :package do
-    set :bundle_name, "#{get(:application)}-#{get(:sbt_version)}.tar.gz"
-    set :bundle_path, "target/#{get(:bundle_name)}"
-    run_locally "tar -czv --file=#{get(:bundle_path)} config cookbooks roles -C target/#{get(:sbt_build_version)} #{get(:sbt_assembly_filename)}"
-  end
-
-  task :update_code do
-    run "mkdir #{get(:release_path)}"
-    put File.read(get(:bundle_path)), get(:release_path) + "/" + get(:bundle_name)
-    run "cd #{get(:release_path)} && tar zxvf #{get(:bundle_name)}"
-  end
-end
-
 namespace :chef do
   task :write_json, :serial => true do
     role = @current_servers.first.role
